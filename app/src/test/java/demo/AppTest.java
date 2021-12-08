83c938be8a4aaf996db91f7c8cd3d59455594b1d
Ugo Proietti <ugo.pr@protonmail.com>
2021-12-08 10:58:50 +0100

    Update AppTest.java

--- app/src/test/java/demo/AppTest.java
+++ app/src/test/java/demo/AppTest.java
@@ -3,8 +3,8 @@
  */
 package demo;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test public void testAppHasAGreeting() {
